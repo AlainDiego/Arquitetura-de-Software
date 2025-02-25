64644b2931940b5f8fe93721cec19b15e65f9107
AlainDiego <alain.passos@outlook.com>
2025-02-24 21:44:51 -0300

    Copiando o Luan

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
 const rotas = {
     "/": "API com Node e Express.js",
-};
+    "/posts": "Rota de postagem",
+    "/autores": "Rota de autores",
+}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url] || "Rota não encontrada");
-});
+//const server = http.createServer((req, res) => {
+//    res.writeHead(200, {"Content-Type": "text/plain" });
+//});
 
-server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT, () => {
+    console.log("Servidor na escuta!");
 });
